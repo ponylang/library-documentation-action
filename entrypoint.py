3fc067029bdd3a0323cd9da489550743ad2d308b
Sean T. Allen <sean@seantallen.com>
2021-01-23 13:54:59 -0500

    Don't reset

--- entrypoint.py
+++ entrypoint.py
@@ -141,12 +141,10 @@ github_token  = os.environ['GITHUB_TOKEN']
 remote = 'https://' + github_token + '@github.com/' + os.environ['GITHUB_REPOSITORY']
 git.remote('add', 'gh-token', remote)
 git.fetch('gh-token')
-git.reset('gh-token/generated-documentation')
 
 os.chdir(docs_build_dir)
 rslt = os.system('mkdocs gh-deploy --verbose --clean \
     --remote-name gh-token --remote-branch generated-documentation')
-
 if rslt != 0:
     print(ERROR + "'mkdocs gh-deploy' failed." + ENDC)
     sys.exit(1)
